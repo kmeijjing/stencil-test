b45db3399068fac1e8c3e52b0b2fec942b16b732
MeeKyeong Kim <kmeijjing@gmail.com>
2025-10-24 18:21:17 +0900

    feat: update TypeScript configuration and fix component exports

--- packages/react/lib/index.ts
+++ packages/react/lib/index.ts
@@ -1,4 +1,4 @@
 "use strict";
 
-export * from "./components/stencil-generated";
+export * from "./components/stencil-generated/index/ts/components";
 export { defineCustomElements } from "@stencil-test/stencil/loader";
